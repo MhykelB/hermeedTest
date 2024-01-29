38e8be4e83cefdd4180703009ec5fd0469d1f2f9
Abdulhameed <busarioyewole@gmail.com>
2024-01-29 15:31:07 +0100

    update

--- src/services/invoiceGenerator.ts
+++ src/services/invoiceGenerator.ts
@@ -4,7 +4,7 @@ import path from "path";
 import hbs from "handlebars";
 import dayjs from "dayjs";
 import "dotenv/config";
-import { API_URL } from "../../app";
+import { API_URL } from "../..";
 
 hbs.registerHelper("dateFormat", function (value, format) {
   return dayjs(value).format(format);
